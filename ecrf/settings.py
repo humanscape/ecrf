d44a59bb081a874dfc943dbadab0bf0a8f311a0b
ted <ted@humanscape.io>
2021-06-23 18:55:06 +0900

    feat: django-environ added

--- ecrf/settings.py
+++ ecrf/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import environ
 from pathlib import Path
+from os.path import join, dirname, isfile, abspath
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+env_file = join(BASE_DIR, 'env/.env')
+if isfile(env_file):
+    env.read_env(env_file=env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
