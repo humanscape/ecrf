132d6e88fbc26b252ea2407929dfb184e466f7f8
ted <ted@humanscape.io>
2021-11-29 14:50:30 +0900

    feat: 데이터 자동 생성을 위한 간단한 스크립트

--- script/model_generator.py
+++ script/model_generator.py
@@ -1,6 +1,6 @@
 import csv
 
-f = open('ird_history.csv', 'r')
+f = open('nf1.csv', 'r')
 data = list(csv.reader(f))
 f.close()
 
@@ -14,19 +14,25 @@ field_map = {
     '선택이있는데이터(choices)': 'models.IntegerField',
 }
 
+print([row[0] for row in data[2:]])
+
 for row in data[2:]:
     field = {
         'column_name': row[0],
         'display_name': row[1],
-        'help_text': row[2],
-        'field_type': row[3]
+        'help_text': row[2].replace("\n", "<br/>"),
+        'field_type': row[3],
     }
+    if len(row[6]) > 0:
+        field['choice_count'] = int(row[6]) + 1
+    else:
+        field['choice_count'] = 0
     # print(field)
 
     # TODO(좀 더 이쁘게 바꾸고싶음..)
     field_text = f"{field['column_name']} = {field_map[field['field_type']]}('{field['display_name']}'"
     if field['field_type'] == '선택이있는데이터(choices)':
-        field_text += f", choices=CHOICES, null=True, blank=True"
+        field_text += f", choices=[(i, i) for i in range({field['choice_count']})], null=True, blank=True"
     elif field['field_type'] == '소수점이있는실수(decimal)':
         field_text += f", max_digits=10, decimal_places=2, null=True, blank=True"
     else:
