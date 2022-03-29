0ed7dd2830b85b49661ca38fca16713928fedb73
ted <ted@humanscape.io>
2022-03-29 18:39:31 +0900

    feat: nf1 수정된 내용 반영

--- nf1/assets.py
+++ nf1/assets.py
@@ -1,19 +1,19 @@
 DISPLAY_FIELDS={
     'crf': (
-        'patient_number', 'family_hx',
-        'sex', 'birth_year_and_month', 'age','date_at_dx', 'age_at_dx', 'date_at_evaluation',
-        'age_at_evaluation', 'nf1_mutation', 'dna', 'protein', 'domain', 'mutation_type',
+        'patient_number',
+        'birth_year_and_month', 'age', 'date_at_dx', 'age_at_dx', 'sex', 'family_hx', 'familyhistory_diagnosis'
+        'date_at_evaluation', 'age_at_evaluation', 'nf1_mutation', 'dna', 'protein', 'domain', 'mutation_type',
         'inframe_deletion_or_insertion', 'nf1_haploinsufficiency_type', 'novel_mutation',
         'clinical_findings', 'cafe_au_lait_spots', 'axillary_freckling', 'cutaneous_neurofibromas',
         'wide_spread_diffuse_cutaneous_neurofibroma', 'relative_macrocephaly', 'lish_nodules',
         'height_at_dx','height_sds', 'learning_difficulty', 'adhd', 'autism', 'seizure', 'hypertension',
-        'cardiac_arrhythmia', 'cardiac_myopathy', 'cardiac_myopathy_findings',
-        'oh_25_vitamin_d', 'hearing_difficulty', 'other', 'brain_mr_date', 'age_at_brain_mr',
-        'mr_findings', 'fasi', 'fasi_findings', 'optic_pathway_glioma', 'vascular_anomaly', 'spine_mr_date',
-        'age_at_spine_mr', 'age_at_spine_mr_findings', 'whole_body_mr_date', 'age_at_whole_body_mr',
+        'cardiac_arrhythmia', 'arrhythmia_treatment', 'cardiac_myopathy', 'cardiac_myopathy_findings',
+        'oh_25_vitamin_d', 'oh_25_vitamin_d_1', 'hearing_difficulty', 'other', 'brain_mr_date', 'age_at_brain_mr',
+        'mr_findings', 'fasi', 'fasi_findings', 'optic_pathway_glioma', 'vascular_anomaly', 'spine_mr', 'spine_mr_date',
+        'age_at_spine_mr', 'age_at_spine_mr_findings', 'whole_body_mr', 'whole_body_mr_date', 'age_at_whole_body_mr',
         'age_at_whole_body_mr_findings', 'plexiform_neurofibromas', 'plexiform_neurofibromas_3',
         'disfigurement', 'aorta_bone_disruption_malignancy', 'painful_accompanying', 'malignancy',
-        'brain_tumor', 'nerve_root_tumor', 'malignant_peripheral_nerve_sheath_tumor', 'moyamoya_disease',
+        'brain_tumor', 'nerve_root_tumor', 'malignant_peripheral_nerve_sheath_tumor', 'moyamoya_disease', 'bmd', 'bmd_1'
         'spine_z_score', 'femur_z_score', 'dysplasia_of_long_bone',
         'dysplasia_of_long_bone_location', 'sphenoid_wing_dysplaisa', 'vertebral_dysplasia',
         'dural_ectasia', 'scoliosis', 'breast_examination', 'date_at_breast_usg', 'age_at_breast_usg',
@@ -27,10 +27,11 @@ RADIO_FIELDS={
         'family_hx', 'sex', 'nf1_mutation', 'domain', 'mutation_type', 'inframe_deletion_or_insertion',
         'nf1_haploinsufficiency_type', 'novel_mutation', 'cafe_au_lait_spots', 'axillary_freckling', 'cutaneous_neurofibromas',
         'wide_spread_diffuse_cutaneous_neurofibroma', 'relative_macrocephaly', 'lish_nodules', 'learning_difficulty', 
-        'adhd', 'autism', 'lish_nodules', 'seizure', 'hypertension', 'cardiac_arrhythmia', 'cardiac_myopathy', 
-        'hearing_difficulty', 'fasi', 'optic_pathway_glioma', 'vascular_anomaly', 'plexiform_neurofibromas', 
+        'adhd', 'autism', 'lish_nodules', 'seizure', 'hypertension', 'cardiac_arrhythmia', 'cardiac_myopathy',
+        'oh_25_vitamin_d_1', 'hearing_difficulty', 'fasi', 'optic_pathway_glioma', 'vascular_anomaly', 'spine_mr',
+        'whole_body_mr', 'plexiform_neurofibromas',
         'plexiform_neurofibromas_3', 'disfigurement', 'aorta_bone_disruption_malignancy', 'painful_accompanying', 'malignancy', 
-        'brain_tumor', 'nerve_root_tumor', 'malignant_peripheral_nerve_sheath_tumor', 'moyamoya_disease',
+        'brain_tumor', 'nerve_root_tumor', 'malignant_peripheral_nerve_sheath_tumor', 'moyamoya_disease', 'bmd', 'bmd_1'
         'dysplasia_of_long_bone', 'sphenoid_wing_dysplaisa', 'vertebral_dysplasia', 'dural_ectasia', 'scoliosis', 
         'breast_examination', 'birads_i_ii_iii_iv', 'biopsy', 'operation'
     )
