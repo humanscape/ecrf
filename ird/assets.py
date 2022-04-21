59e5e52d770ed6395cf3a5b2dfd0c6fe5bd74180
ted <ted@humanscape.io>
2022-04-21 12:10:21 +0900

    feat: 라디오 필드 추가

--- ird/assets.py
+++ ird/assets.py
@@ -7,7 +7,10 @@ RADIO_FIELDS={
         'cystoid_macular_edema_os', 'sector_rp_os', 'retinitis_punctata_albescens_os',
         'night_blindness', 'peripheral_vision', 'central_vision', 'read',
         'object_recognition', 'cataract', 'glaucoma', 'retinal_detachment', 'dark_adaptation', 
-        'photopsia', 'color_sense'
+        'photopsia', 'color_sense', 'familyhistory_diagnosis2', 'drug', 'myodesopsia',
+        'night_blindness', 'peripheral_vision', 'central_vision', 'read',
+        'object_recognition', 'cataract', 'glaucoma', 'retinal_detachment',
+        'dark_adaptation', 'photopsia', 'color_sense', 'dazzling', 'hearing_defect'
     )
 }
 
