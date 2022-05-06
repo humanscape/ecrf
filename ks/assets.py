d3415a1258a8c67a06d876116afb45277fd5a623
ted <ted@humanscape.io>
2022-05-06 16:45:00 +0900

    feat: asset 추가

---
+++ ks/assets.py
@@ -0,0 +1,24 @@
+DISPLAY_FIELDS={
+    'ks': (
+    )
+}
+
+RADIO_FIELDS={
+    'ks': (
+    )
+}
+
+PREFIX={
+    'ks': {
+    }
+}
+
+SUFFIX={
+    'Ks': {
+    }
+}
+
+CALCULATE_AGE_FIELDS={
+    'ks': {
+    }
+}
